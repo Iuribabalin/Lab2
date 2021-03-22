fba831f17a29aa150c5e908705c0704994afb500
Iuribabalin <babalin2011@gmail.com>
2021-03-22 12:43:57 +0300

    3

--- src/main/java/lab/Inputs/CheckInterval.java
+++ src/main/java/lab/Inputs/CheckInterval.java
@@ -1,101 +1,29 @@
 package lab.Inputs;
 
-import lab.Functions.*;
+import lab.Functions.All_fun;
 
 public class CheckInterval {
-    Main_fun main_fun = null;
-    Cos_fun cos_fun = null;
-    Parabola_fun parabola_fun = null;
-    Sin_fun sin_fun = null;
+    All_fun fun = new All_fun();
 
     public boolean check(double a, double b, int num_fun){
-        if(num_fun == 1){
-            main_fun = new Main_fun();
-            for(double i = a; i <= b-0.001; i=i+0.001){
-                if(main_fun.f(i) > 0 && main_fun.f(i+0.001) < 0 || main_fun.f(i) < 0 && main_fun.f(i+0.001) > 0){
-                    return true;
-                }
+
+        for(double i = a; i <= b-0.001; i=i+0.001){
+            if(fun.f(i,num_fun) > 0 && fun.f(i+0.001, num_fun) < 0 ||
+                    fun.f(i,num_fun) < 0 && fun.f(i+0.001, num_fun) > 0){
+                return true;
             }
-
-            return main_fun.f(a) * main_fun.f(b) < 0;
-
-        }else if(num_fun == 2){
-            cos_fun = new Cos_fun();
-
-            for(double i = a; i <= b-0.001; i=i+0.001){
-                if(cos_fun.f(i) > 0 && cos_fun.f(i+0.001) < 0 ||
-                        cos_fun.f(i) < 0 && cos_fun.f(i+0.001) > 0){
-                    return true;
-                }
-            }
-
-            return cos_fun.f(a) * cos_fun.f(b) < 0;
-
-        }else if(num_fun == 3){
-            parabola_fun = new Parabola_fun();
-
-            for(double i = a; i <= b-0.001; i=i+0.001){
-                if(parabola_fun.f(i) > 0 && parabola_fun.f(i+0.001) < 0 ||
-                        parabola_fun.f(i) < 0 && parabola_fun.f(i+0.001) > 0){
-                    return true;
-                }
-            }
-
-            return parabola_fun.f(a) * parabola_fun.f(b) < 0;
-
-        }else if(num_fun == 4){
-            sin_fun = new Sin_fun();
-
-            for(double i = a; i <= b-0.001; i=i+0.001){
-                if(sin_fun.f(i) > 0 && sin_fun.f(i+0.001) < 0 ||
-                        sin_fun.f(i) < 0 && sin_fun.f(i+0.001) > 0){
-                    return true;
-                }
-            }
-
-            return sin_fun.f(a) * sin_fun.f(b) < 0;
         }
 
-        return false;
+        return fun.f(a,num_fun) * fun.f(b,num_fun) < 0;
     }
 
     public int checkCntAnswer(double a, double b, int num_fun){
         int cnt = 0;
-        if(num_fun == 1){
-            main_fun = new Main_fun();
-            for(double i = a; i <= b-0.001; i=i+0.001){
-                if(main_fun.f(i) > 0 && main_fun.f(i+0.001) < 0 ||
-                        main_fun.f(i) < 0 && main_fun.f(i+0.001) > 0){
-                    cnt++;
-                }
-            }
-        }else if(num_fun == 2){
-            cos_fun = new Cos_fun();
 
-            for(double i = a; i <= b-0.001; i=i+0.001){
-                if(cos_fun.f(i) > 0 && cos_fun.f(i+0.001) < 0 ||
-                        cos_fun.f(i) < 0 && cos_fun.f(i+0.001) > 0){
-                    cnt++;
-                }
-            }
-        }else if(num_fun == 3){
-            parabola_fun = new Parabola_fun();
-
-            for(double i = a; i <= b-0.001; i=i+0.001){
-                if(parabola_fun.f(i) > 0 && parabola_fun.f(i+0.001) < 0 ||
-                        parabola_fun.f(i) < 0 && parabola_fun.f(i+0.001) > 0){
-                    cnt++;
-                }
-            }
-
-        }else if(num_fun == 4){
-            sin_fun = new Sin_fun();
-
-            for(double i = a; i <= b-0.001; i=i+0.001){
-                if(sin_fun.f(i) > 0 && sin_fun.f(i+0.001) < 0 ||
-                        sin_fun.f(i) < 0 && sin_fun.f(i+0.001) > 0){
-                    cnt++;
-                }
+        for(double i = a; i <= b-0.001; i=i+0.001){
+            if(fun.f(i,num_fun) > 0 && fun.f(i+0.001,num_fun) < 0 ||
+                    fun.f(i,num_fun) < 0 && fun.f(i+0.001,num_fun) > 0){
+                cnt++;
             }
         }
 
